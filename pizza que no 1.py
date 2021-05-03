f5a2bc5d9bf080af0cfe86e868541151148c9402
akssm <35273549+akssm@users.noreply.github.com>
2021-05-03 18:28:10 +0530

    Add files via upload

---
+++ pizza que no 1.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+print("\t\t\t\t! Welcome to Desire's Pizza Deliveries !\n")
+
+while True:
+     num = (input("Enter Y to Proceed with bill for New customer: "))
+     if(num == 'Y' or num == 'y'):
+         name = (input("Enter the Name of the customer:--> "))
+         pizza_size = input("What size pizza do you want? S, M, or L :--> ")
+         
+         
+         
+         pizza_bill = 0
+         if (pizza_size == "S" or pizza_size == "s"):
+                 pizza_bill += 150
+         elif (pizza_size == "M" or pizza_size == "m"):
+            pizza_bill += 200
+         elif (pizza_size == "L" or pizza_size == "l"):
+            pizza_bill += 250
+    
+            
+         if (pizza_size == "S" or pizza_size == "s" or pizza_size == "M" or pizza_size == "m" or pizza_size == "L" or pizza_size == "l" ):
+             add_pepperoni = input("Do you want pepperoni? Y or N :--> ")
+             extra_cheese = input("Do you want extra cheese? Y or N :--> ")
+             pizza_bread = input("Do you want thin bread? Y or N :--> ")
+             if (add_pepperoni == "Y" or add_pepperoni == "y"):
+                  pizza_bill += 20
+             
+             if (extra_cheese == "Y" or extra_cheese == "y"):
+                 pizza_bill += 10
+                 
+               
+             
+             if (pizza_bread == "Y" or pizza_bread == "y"):
+                 pizza_bill += 50
+                 
+                 
+                 
+             print("Name: ",name)   
+             print("Your final Bill is: Rs.",pizza_bill)    
+         else:
+            print("\n")
+             
+            print('''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣶⣶⣦⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣷⣤⠀⠈⠙⢿⣿⣿⣿⣿⣿⣦⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⠆⠰⠶⠀⠘⢿⣿⣿⣿⣿⣿⣆⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⣿⣿⠏⠀⢀⣠⣤⣤⣀⠙⣿⣿⣿⣿⣿⣷⡀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⢠⠋⢈⣉⠉⣡⣤⢰⣿⣿⣿⣿⣿⣷⡈⢿⣿⣿⣿⣿⣷⡀
+⠀⠀⠀⠀⠀⠀⠀⡴⢡⣾⣿⣿⣷⠋⠁⣿⣿⣿⣿⣿⣿⣿⠃⠀⡻⣿⣿⣿⣿⡇
+⠀⠀⠀⠀⠀⢀⠜⠁⠸⣿⣿⣿⠟⠀⠀⠘⠿⣿⣿⣿⡿⠋⠰⠖⠱⣽⠟⠋⠉⡇
+⠀⠀⠀⠀⡰⠉⠖⣀⠀⠀⢁⣀⠀⣴⣶⣦⠀⢴⡆⠀⠀⢀⣀⣀⣉⡽⠷⠶⠋⠀
+⠀⠀⠀⡰⢡⣾⣿⣿⣿⡄⠛⠋⠘⣿⣿⡿⠀⠀⣐⣲⣤⣯⠞⠉⠁⠀⠀⠀⠀⠀
+⠀⢀⠔⠁⣿⣿⣿⣿⣿⡟⠀⠀⠀⢀⣄⣀⡞⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⡜⠀⠀⠻⣿⣿⠿⣻⣥⣀⡀⢠⡟⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢰⠁⠀⡤⠖⠺⢶⡾⠃⠀⠈⠙⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠈⠓⠾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀''')
+            print("\n\n")
+            print("Uff Sorry ! Please enter correct Pizza Size !")
+            
+           
+    
+     continue
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
